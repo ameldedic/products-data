c59ddf1a45bdf710dbf52cf6b72777c8dec20f43
Amel Dedic <amel_dedic@hotmail.com>
2019-03-11 21:18:43 -0400

    Create data.php

---
+++ data.php
@@ -0,0 +1,38 @@
+$products = [
+    [
+        "id" => "tb",
+        "thumbnail" => "http://via.placeholder.com/300/ffff00/000000/?text=Table+King",
+        "type" => "Kitchen table King",
+        "price" => 250
+    ],
+    [
+        "id" => "tb",
+        "thumbnail" => "http://via.placeholder.com/300/ffff00/000000/?text=Table+Cindirela",
+        "type" => "Kitchen table Cindirela",
+        "price" => 350
+    ],
+    [
+        "id" => "kc",
+        "thumbnail" => "http://via.placeholder.com/300/ff00ff/ffffff/?text=Chair+King",
+        "type" => "Kitchen Chair King",
+        "price" => 100
+    ],
+    [
+        "id" => "kc",
+        "thumbnail" => "http://via.placeholder.com/300/ff00ff/ffffff/?text=Chair+Cindirela",
+        "type" => "Kitchen Chair Cindirela",
+        "price" => 150
+    ],
+    [
+        "id" => "kcb",
+        "thumbnail" => "http://via.placeholder.com/300/00ffff/000000/?text=Cabinet+King",
+        "type" => "Kitchen Cabinet King",
+        "price" => 300
+    ],
+    [
+        "id" => "kcb",
+        "thumbnail" => "http://via.placeholder.com/300/00ffff/000000/?text=Cabinet+Cindirela",
+        "type" => "Kitchen Cabinet Cindirela",
+        "price" => 350
+    ]
+];
